34bacfb441c925340d000268e9513c5a236966fc
Matteo <rosella.matteo@gmail.com>
2020-05-11 14:39:50 +0200

    Milestone 1 e creazione cartelle Ms2 e Ms3

---
+++ milestone1/database.php
@@ -0,0 +1,8 @@
+<?php
+  $data = [
+    data => [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322],
+    mesi => ['Gennaio' , 'Febbraio' , 'Marzo' , 'Aprile' , 'Maggio' , 'Giugno' , 'Luglio' , 'Agosto' , 'Settembre' , 'Ottobre' , 'Novembre' , 'Dicembre']
+  ];
+  header('Content-Type: application/json');
+  echo json_encode($data);
+?>
---
+++ milestone1/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  var ctx = document.getElementById('line').getContext('2d');
+  $.ajax({
+    url: 'database.php',
+    method: 'GET',
+    success: function success(data) {
+      var mesi = data.mesi;
+      var dati = data.data;
+      var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: mesi,
+              datasets: [{
+                  label: '# of Votes',
+                  data: dati,
+                  backgroundColor: [
+                      'rgba(219, 193, 193, 0.8)',
+                  ],
+                  borderColor: [
+                      'rgba(0, 0, 0, 0.9)',
+                      'rgba(0, 120, 92, 1)',
+                      'rgba(0, 234, 255, 1)',
+                      'rgba(99, 180, 255, 1)',
+                      'rgba(77, 100, 189, 1)',
+                      'rgba(255, 0, 0, 1)',
+                      'rgba(255, 100, 43, 1)',
+                      'rgba(193, 34, 214, 1)',
+                      'rgba(141, 168, 240, 1)',
+                      'rgba(145, 235, 169, 1)',
+                      'rgba(242, 189, 232, 1)',
+                      'rgba(40, 99, 20, 1)',
+                  ],
+                  borderWidth: 3
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+    },
+    error: function error(err) {}
+  });
+})
