1cad44d263ec96cc6d0bbdd6f041ba0fceeef211
Matteo <rosella.matteo@gmail.com>
2020-05-12 21:22:54 +0200

    Milestone 2

--- milestone2/script.js
+++ milestone2/script.js
@@ -44,31 +44,31 @@ $(document).ready(function () {
               }
           }
       });
-      var valori = data.fatturato_by_agent.data;
+      var valori = [
+        data.fatturato_by_agent.data.Marco,
+        data.fatturato_by_agent.data.Giuseppe,
+        data.fatturato_by_agent.data.Mattia,
+        data.fatturato_by_agent.data.Alberto
+        ]
       console.log(valori);
       var myChart = new Chart(fetta, {
           type: data.fatturato_by_agent.type,
           data: {
-              labels: valori,
+              labels: ['Marco', 'Giuseppe', 'Mattia', 'Alberto'],
               datasets: [{
                   label: '# of Votes',
-                  data: [300],
+                  data: valori,
                   backgroundColor: [
-                      'rgba(219, 193, 193, 0.8)',
+                      'rgba(9, 72, 173, 0.9)',
+                      'rgba(173, 9, 148, 0.9)',
+                      'rgba(9, 173, 80, 0.9)',
+                      'rgba(196, 51, 22, 0.9)',
                   ],
                   borderColor: [
-                      'rgba(0, 0, 0, 0.9)',
-                      'rgba(0, 120, 92, 1)',
-                      'rgba(0, 234, 255, 1)',
-                      'rgba(99, 180, 255, 1)',
-                      'rgba(77, 100, 189, 1)',
-                      'rgba(255, 0, 0, 1)',
-                      'rgba(255, 100, 43, 1)',
-                      'rgba(193, 34, 214, 1)',
-                      'rgba(141, 168, 240, 1)',
-                      'rgba(145, 235, 169, 1)',
-                      'rgba(242, 189, 232, 1)',
-                      'rgba(40, 99, 20, 1)',
+                      'rgba(0, 0, 0, 0.6)',
+                      'rgba(0, 0, 0, 0.6)',
+                      'rgba(0, 0, 0, 0.6)',
+                      'rgba(0, 0, 0, 0.6)',
                   ],
                   borderWidth: 3
               }]
--- milestone2/server.php
+++ milestone2/server.php
@@ -2,4 +2,5 @@
   include 'database.php';
   header('Content-Type: application/json');
   echo json_encode($graphs);
+
  ?>
